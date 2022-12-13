3daa64ce5dae4ed64b598fe567258fbb9f3100b6
carolinag11 <118641406+carolinag11@users.noreply.github.com>
2022-12-12 22:06:25 -0500

    Update Ejercicio 3 Fase 2.sql

--- Modulo_1/Ejercicio 3 Fase 2.sql
+++ Modulo_1/Ejercicio 3 Fase 2.sql
@@ -1,27 +1,17 @@
-WITH
-Interactions_Active as(
 SELECT 
 date_trunc('month', date(a.dt)) as fecha, 
-act_acct_cd
+count(distinct act_acct_cd) as users
 FROM "db-analytics-prod"."fixed_cwp" a
 inner join "db-stage-prod"."interactions_cwp" b
 on a.act_acct_cd=b.account_id
 and date_trunc('month', date(a.dt))=date_trunc('month', date(b.interaction_start_time))
-where act_cust_typ_nm='Residencial'
+inner join "db-stage-dev"."so_hdr_cwp" c
+on a.act_acct_cd=cast(c.account_id as varchar)
+and date_trunc('month', date(date_trunc('month', date(a.dt))))=date_trunc('month', date(c.order_start_date))
+where date_trunc('month', date(order_start_date))>=date('2022-01-01')
+and act_cust_typ_nm='Residencial'
 and date_trunc('month', date(a.dt))>=date('2022-01-01') 
 and date(a.dt)=date_trunc('month', date(a.dt))
 and date_trunc('month', date(interaction_start_time))>=date('2022-01-01') 
-group by 1,2
-order by 1 asc
-)
-
-select
-fecha,
-count(distinct act_acct_cd) as users
-from Interactions_Active a
-inner join "db-stage-dev"."so_hdr_cwp" b
-on a.act_acct_cd=cast(b.account_id as varchar)
-and date_trunc('month', date(a.fecha))=date_trunc('month', date(b.order_start_date))
-where date_trunc('month', date(order_start_date))>=date('2022-01-01')
 group by 1
-order by 1
+order by 1 asc

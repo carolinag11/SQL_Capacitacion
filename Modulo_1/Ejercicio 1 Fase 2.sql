4a3510bf9ac78f89196922cc17f860a4aadcf7bd
carolinag11 <118641406+carolinag11@users.noreply.github.com>
2022-12-12 21:37:33 -0500

    Update Ejercicio 1 Fase 2.sql

--- Modulo_1/Ejercicio 1 Fase 2.sql
+++ Modulo_1/Ejercicio 1 Fase 2.sql
@@ -1,32 +1,15 @@
-WITH 
-Truckrolls as (
-select
-account_id,
-date_trunc('month', date(interaction_start_time)) as fecha1,
-interaction_purpose_descrip,
-concat(cast(date_trunc('month', date(interaction_start_time))as varchar),'/',account_id) as concatenado
-from "db-stage-prod"."interactions_cwp" 
-where interaction_purpose_descrip='TRUCKROLL' 
-and date_trunc('month', date(interaction_start_time))>=date('2022-01-01') 
-)
-,
-Users as(
 SELECT 
-date_trunc('month', date(dt)) as fecha, 
-act_acct_cd,
-concat(cast(date_trunc('month', date(dt))as varchar),'/',act_acct_cd) as concatenado
-FROM "db-analytics-prod"."fixed_cwp" 
+date_trunc('month', date(a.dt)) as fecha, 
+count(distinct act_acct_cd) as users
+FROM "db-analytics-prod"."fixed_cwp" a
+inner join "db-stage-prod"."interactions_cwp" b
+on a.act_acct_cd=b.account_id
+and date_trunc('month', date(a.dt))=date_trunc('month', date(b.interaction_start_time))
 where act_cust_typ_nm='Residencial'
-and date_trunc('month', date(dt))>=date('2022-01-01') 
-and date(dt)=date_trunc('month', date(dt))
-  and pd_bb_prod_cd is not null
-order by 1 asc
-)
-
-select
-a.fecha,
-count(distinct a.act_acct_cd) as users
-from Users a
-inner join Truckrolls b on a.concatenado=b.concatenado
+and date_trunc('month', date(a.dt))>=date('2022-01-01') 
+and date(a.dt)=date_trunc('month', date(a.dt))
+and pd_bb_prod_cd is not null
+and interaction_purpose_descrip='TRUCKROLL' 
+and date_trunc('month', date(interaction_start_time))>=date('2022-01-01') 
 group by 1
-order by 1
+order by 1 asc

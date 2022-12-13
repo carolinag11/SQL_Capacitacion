5d2e64e352de81b2c74d799767084285ed2dc7f5
carolinag11 <118641406+carolinag11@users.noreply.github.com>
2022-12-12 21:59:56 -0500

    Update Ejercicio 3 Fase 2.sql

--- Modulo_1/Ejercicio 3 Fase 2.sql
+++ Modulo_1/Ejercicio 3 Fase 2.sql
@@ -1,39 +1,27 @@
 WITH
-Interactions as(
-select
-account_id,
-date_trunc('month', date(interaction_start_time)) as fecha1,
-interaction_purpose_descrip,
-concat(cast(date_trunc('month', date(interaction_start_time))as varchar),'/',account_id) as concatenado
-from "db-stage-prod"."interactions_cwp" 
-where date_trunc('month', date(interaction_start_time))>=date('2022-01-01') 
-)
-,
-Orders as(
-select
-cast(account_id as varchar) as account_id,
-date(date_trunc('month',order_start_date)) as fecha,
-concat(cast(date_trunc('month', date(order_start_date))as varchar),'/',cast(account_id as varchar)) as concatenado
-from "db-stage-dev"."so_hdr_cwp"
-),
-
-Users as(
+Interactions_Active as(
 SELECT 
-date_trunc('month', date(dt)) as fecha, 
-act_acct_cd,
-concat(cast(date_trunc('month', date(dt))as varchar),'/',act_acct_cd) as concatenado
-FROM "db-analytics-prod"."fixed_cwp" 
+date_trunc('month', date(a.dt)) as fecha, 
+act_acct_cd
+FROM "db-analytics-prod"."fixed_cwp" a
+inner join "db-stage-prod"."interactions_cwp" b
+on a.act_acct_cd=b.account_id
+and date_trunc('month', date(a.dt))=date_trunc('month', date(b.interaction_start_time))
 where act_cust_typ_nm='Residencial'
-and date_trunc('month', date(dt))>=date('2022-01-01') 
-and date(dt)=date_trunc('month', date(dt))
+and date_trunc('month', date(a.dt))>=date('2022-01-01') 
+and date(a.dt)=date_trunc('month', date(a.dt))
+and date_trunc('month', date(interaction_start_time))>=date('2022-01-01') 
+group by 1,2
 order by 1 asc
 )
 
 select
-a.fecha1,
-count(distinct a.account_id) as users
-from Interactions a
-inner join Orders b on a.concatenado=b.concatenado
-inner join Users c on a.concatenado=c.concatenado
+fecha,
+count(distinct act_acct_cd) as users
+from Interactions_Active a
+inner join "db-stage-dev"."so_hdr_cwp" b
+on a.act_acct_cd=cast(b.account_id as varchar)
+and date_trunc('month', date(a.fecha))=date_trunc('month', date(b.order_start_date))
+where date_trunc('month', date(order_start_date))>=date('2022-01-01')
 group by 1
 order by 1
